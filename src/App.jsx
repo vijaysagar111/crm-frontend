e555ead0188bf1396d48b64dae1af45228862aed
vijaysagar111 <sagarvijay.vs@gmail.com>
2021-02-23 16:04:13 +0530

    Linking fixed

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Link, Redirect } from 'react-router-dom';
 import { signout } from './helpers/auth';
 import { ToastContainer, toast } from 'react-toastify';
 import {  isAuth } from './helpers/auth';
+import './App.css'
 function App({ history }) {
   return (
     <div className='min-h-screen bg-gray-100 text-gray-900 flex justify-center'>
@@ -19,7 +20,7 @@ function App({ history }) {
                   Features
                 </div>
               </div>
-              <div className='mx-auto max-w-xs relative'>
+              <div className='mx-auto max-w-sm btn relative'>
                 <Link
                   to='/login'
                   className='mt-5 tracking-wide font-semibold bg-indigo-500 text-gray-100 w-full py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none'
